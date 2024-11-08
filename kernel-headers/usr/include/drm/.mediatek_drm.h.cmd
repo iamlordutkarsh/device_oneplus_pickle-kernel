983a81bf78aa50a41836ecb609600cd13763ac9b
iamlordutkarsh <lalbaake456@gmail.com>
2024-11-08 13:50:34 +0530

    pickle-kernel: Import kernel-headers from Oxygen Os 14

---
+++ kernel-headers/usr/include/drm/.mediatek_drm.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/drm/mediatek_drm.h := sh ./scripts/headers_install.sh include/uapi/drm/mediatek_drm.h usr/include/drm/mediatek_drm.h
---
+++ kernel-headers/usr/include/linux/.mtk_ccd_controls.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/mtk_ccd_controls.h := sh ./scripts/headers_install.sh include/uapi/linux/mtk_ccd_controls.h usr/include/linux/mtk_ccd_controls.h
